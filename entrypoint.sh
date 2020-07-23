6b72a814887a7e06350ce7f3cad26947d973b445
Tesch, Aaron <aaron.tesch@accenture.com>
2020-07-23 14:52:10 -0500

    update entryppoint to find and scan all CFTs

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#cleanup
+
+rm /tmp/cfn_templates
+rd /tmp/cfn_guard_temp
+
+grep -r --exclude="entrypoing.sh" 'AWSTemplateFormatVersion' ./* |cut -d':' -f1 > /tmp/cfn_templates
+count=0
+mkdir /tmp/cfn_guard_temp
+for x in $(cat /tmp/cfn_templates); do
+    echo "Scanning file - ${x}"
+    cfn-guard -vt ${x} -r /rules/cfn-guard-sg-rule-testing.ruleset > /tmp/cfn_guard_temp/results${count}.json
+    cat /tmp/cfn_guard_temp/results${count}.json
+    count=`expr $count + 1`
+done
+#python3 build_results_file.py
