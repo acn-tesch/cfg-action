55bcf4ba819bea49ec13fb59784dbc1bead06d2d
Tesch, Aaron <aaron.tesch@accenture.com>
2020-07-27 08:54:48 -0500

    type on entrypoint, and ignores on cft folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@
 rm /tmp/cfn_templates
 rd /tmp/cfn_guard_temp
 
-grep -r --exclude="entrypoing.sh" 'AWSTemplateFormatVersion' ./* |cut -d':' -f1 > /tmp/cfn_templates
+grep -r --exclude="entrypoint.sh" 'AWSTemplateFormatVersion' ./* |cut -d':' -f1 > /tmp/cfn_templates
 count=0
 mkdir /tmp/cfn_guard_temp
 for x in $(cat /tmp/cfn_templates); do
     echo "Scanning file - ${x}"
-    cfn-guard -vt ${x} -r /rules/cfn-guard-sg-rule-testing.ruleset > /tmp/cfn_guard_temp/results${count}.json
+    cfn-guard -t ${x} -r /rules/cfn-guard-sg-rule-testing.ruleset > /tmp/cfn_guard_temp/results${count}.json
     cat /tmp/cfn_guard_temp/results${count}.json
     count=`expr $count + 1`
 done
